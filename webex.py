31e0413a31d97db0bb895473261dc1a7b97c8bba
amanuelian <manuelianalex.am@gmail.com>
2020-04-24 20:30:17 -0300

    Get team ID

--- webex.py
+++ webex.py
@@ -12,12 +12,12 @@ headers =  {'Authorization': f'Bearer {token}',
             'Content-Type': 'application/json'}
 
 body = {
-    "name": "Room test"
+    "name": "TEAM test"
 }
 
-# post_response = requests.post(
-#     url, headers= headers, data= json.dumps(body)).json()
-# print(post_response)
+post_response = requests.post(
+    url, headers= headers, data= json.dumps(body)).json()
+print(post_response)
 
 
 #### GET LIST OF TEAMS ######
@@ -28,3 +28,12 @@ get_response = requests.get(url, headers= headers).json()
 parsed = json.dumps(get_response, indent=2, sort_keys=True)
 print (parsed)
 
+#### CREATE A ROOM ###
+
+teams = get_response['items']
+
+for team in teams:
+    if team['name'] == "TEAM test":
+        teamID = team['id']
+
+print (f'The team ID is {teamID}')
