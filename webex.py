be7fb7d54b8cdb444b6b2de9d00a94d7837589ca
amanuelian <manuelianalex.am@gmail.com>
2020-04-24 20:25:16 -0300

    Code cleaned and organized

--- webex.py
+++ webex.py
@@ -5,7 +5,7 @@ import pprint
 
 token = 'NDBiYmZiYzgtY2JkMC00MWU2LWE3NTUtYTE2MDcxMDg3NzYzZDhjYzJlMDctMzI2_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
 
-# Create a team
+#### CREATE A TEAM ######
 
 url = 'https://api.ciscospark.com/v1/teams'
 headers =  {'Authorization': f'Bearer {token}',
@@ -19,8 +19,12 @@ body = {
 #     url, headers= headers, data= json.dumps(body)).json()
 # print(post_response)
 
+
+#### GET LIST OF TEAMS ######
+
 get_response = requests.get(url, headers= headers).json()
 
+# Pretty print
 parsed = json.dumps(get_response, indent=2, sort_keys=True)
 print (parsed)
 
