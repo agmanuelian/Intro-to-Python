09161b3c55791f2ff42dfa1ed39619f6cf951222
amanuelian <manuelianalex.am@gmail.com>
2020-04-24 22:26:40 -0300

    Posting a message into a room

--- webex.py
+++ webex.py
@@ -3,7 +3,7 @@ import requests
 import json
 import pprint
 
-token = ############
+token = 'NDBiYmZiYzgtY2JkMC00MWU2LWE3NTUtYTE2MDcxMDg3NzYzZDhjYzJlMDctMzI2_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f'
 
 #### CREATE A TEAM ######
 
@@ -28,12 +28,43 @@ get_response = requests.get(url, headers= headers).json()
 parsed = json.dumps(get_response, indent=2, sort_keys=True)
 print (parsed)
 
-#### CREATE A ROOM ###
-
 # Get team ID
 teams = get_response['items']
 
 for team in teams:
     if team['name'] == "TEAM test":
         teamID = team['id']
+
+#### CREATE A ROOM ###
+
+room_url = 'https://api.ciscospark.com/v1/rooms'
+room_body = {
+    "title": "ROOM test",
+    "teamId": teamID
+}
+
+# post_room = requests.post(
+#     room_url, headers= headers, data= json.dumps(room_body)).json()
+
+get_rooms =  requests.get(room_url, headers= headers).json()
+
+rooms = get_rooms['items']
+
+for room in rooms:
+    if room['title'] == "ROOM test":
+        roomid = room['id']
+        #print (json.dumps(room, indent=2))
+
+#### POST A MESSAGE INTO THE ROOM #####
+
+msg_url = 'https://api.ciscospark.com/v1/messages'
+msg_body= {
+    "roomId": roomid,
+    "text": "This is the seconde test message"
+}
+
+msg_post = requests.post(
+     msg_url, headers= headers, data= json.dumps(msg_body)).json()
+
+print (json.dumps(msg_post, indent=2))
 
