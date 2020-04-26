4ee843da95166dd607e59208f843056a1b34c6a4
amanuelian <manuelianalex.am@gmail.com>
2020-04-26 13:10:15 -0300

    Get AP and EPg

---
+++ aci_request.py
@@ -0,0 +1,50 @@
+import requests
+import json
+
+#### LOGIN ####
+
+url = 'https://sandboxapicdc.cisco.com:443/api/aaaLogin.json'
+
+payload = {
+    'aaaUser': {
+        'attributes': {
+            'name': 'admin',
+            'pwd': 'ciscopsdt'
+        }
+    }
+}
+
+headers= {
+    'Content-Type': 'application/json'
+}
+
+response = requests.post(url, headers=headers, data= json.dumps(payload), verify= False).json()
+
+#print(json.dumps(response, indent=2))
+##### PARSE TOKEN AND SET COOKIE #####
+token = response['imdata'][0]['aaaLogin']['attributes']['token']
+cookie={}
+
+cookie['APIC-cookie'] = token
+
+##### GET APPLICATION PROFILE #####
+
+#url = 'https://sandboxapicdc.cisco.com:443/api/node/mo/uni/tn-Heroes/ap-Save_The_Planet.json'
+
+headers= {'cache-control': 'no-cache'}
+
+# response_epg = requests.get(url, headers= headers, cookies=cookie, verify= False).json()
+
+# print(json.dumps(response_epg, indent=2))
+
+
+##### GET ENDPOINT GROUP  #####
+
+url = 'https://sandboxapicdc.cisco.com:443/api/node/mo/uni/tn-Heroes/ap-Save_The_Planet/epg-web.json'
+
+response_epg = requests.get(url, headers= headers, cookies=cookie, verify= False).json()
+
+print(json.dumps(response_epg, indent=2))
+
+
+
