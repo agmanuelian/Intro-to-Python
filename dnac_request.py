159b2d39445df0c0b621b2d5d6d9583f81970b5f
amanuelian <manuelianalex.am@gmail.com>
2020-04-26 16:12:47 -0300

    Get site health

--- dnac_request.py
+++ dnac_request.py
@@ -1,11 +1,12 @@
 import requests 
 from requests.auth import HTTPBasicAuth
+import json
 
 # Credentials
-user = 'demo'
-pwd = 'demo1234!'
+user = 'devnetuser'
+pwd = 'Cisco123!'
 
-url_auth = 'https://dcloud-dna-center-inst-sjc.cisco.com/dna/system/api/v1/auth/token'
+url_auth = 'https://sandboxdnac2.cisco.com/dna/system/api/v1/auth/token'
 
 ###### GET AUTH TOKEN #####
 headers = {
@@ -13,11 +14,25 @@ headers = {
     #'Authorization': 'Basic ZGVtbzpkZW1vMTIzNCE='
 }
 
-auth_response = requests.post(url_auth, headers=headers, auth= HTTPBasicAuth(user, pwd)).json()
+auth_response = requests.post(url_auth, headers=headers, auth= (user, pwd)).json()
 
 token = auth_response['Token']
 
-print(f'Success! Token is: {token}')
+#print(f'Success! Token is: {token}')
 
+##### GET SITE HEALTH #####
 
+# url_sh = 'https://sandboxdnac2.cisco.com/dna/system/api/v1/site-health'
+
+# headers= {
+#     'x-auth-token': token,
+#     # 'cache-control': 'no-cache',
+#     # 'Host': 'sanboxdnac2.cisco.com',
+#     # 'Accept-Encoding': 'gzip, deflate',
+#     # 'Connection':'keep-alive'
+#     }
+
+# site_health = requests.get(url_sh, headers = headers).json()
+
+# print(json.dumps(site_health, indent=2))
 
