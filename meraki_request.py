5059e7d3e6915324f6aac4affc24127bde8f9f5c
amanuelian <manuelianalex.am@gmail.com>
2020-04-26 18:36:26 -0300

    Get list of network clients

--- meraki_request.py
+++ meraki_request.py
@@ -19,13 +19,21 @@ org_req = requests.get(org_url, headers=headers).json()
 
 # Print list of organizations
 
+print('\n')
+print ('LIST OF ORGANIZATIONS')
 for org in org_req:
     print (f'Name: ' + org['name'])
 
+print('\n')
+
 #### GET AN ORGANIZATION'S LIST OF NETWORKS #####
 
+orgname = 'DeLab'
+
+print (f'LIST OF NETWORKS BELONGING TO NETWORK "{orgname}"')
+
 for org in org_req:
-    if org['name'] == 'DevNet San Jose':
+    if org['name'] == orgname:
         orgid = org['id']
 
 net_url = org_url + '/' + orgid + '/networks' 
@@ -33,18 +41,30 @@ net_url = org_url + '/' + orgid + '/networks'
 org_networks = requests.get(net_url, headers=headers).json()
 
 for net in org_networks:
-    if net['name'] == 'Cisco Live Network':
+    # Print list of networks
+    print (net['name'])
+    if net['name'] == 'Vegas Apartment':
         netid = net['id']
 
+print('\n')
 print (f'Network ID: {netid}')
+
 #### GET NETWORK DEVICES #####
 
 devices_url = url + '/networks/' + netid + '/devices'
 
 devices = requests.get(devices_url, headers=headers).json()
 
-for device in devices:
-    if (device['model'] in device) and (device['lanIp'] in device):
-        print(f'Device: '+ device['model'])
-        print(f'IP Address: ' + device['lanIp'])
-#print(json.dumps(devices, indent=2))
+# for device in devices:
+#     print(f'Device: '+ device['model'])
+#     print(f'IP Address: ' + device['lanIp'] + '\n')
+
+#### GET LIST OF CLIENTS CONNECTED TO THE NETWORK #####
+
+clients_url = url + '/networks/' + netid + '/clients'
+
+clients = requests.get(clients_url, headers=headers).json()
+
+for client in clients:
+    print(f'Client description: ' + client['description']+ '|| IP Address: ' + client['ip'])
+#print(json.dumps(clients, indent=2))
